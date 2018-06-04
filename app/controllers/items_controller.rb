722d5a31d697febaa84a5c110c0418dd7721fd83
vladshkurin <shkurinvlad@gmail.com>
2018-06-04 10:05:59 +0300

    add Item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,16 @@
 class ItemsController < ApplicationController
+  def index
+    @items = Item.all
+    json_response(@items)
+  end
+
+  private
+
+  def item_params
+    params.permit(:title)
+  end
+
+  # def set_item
+  #   @item = Item.find_by(params[:id])
+  # end
 end
